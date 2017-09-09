33d037fac03491dcedba622783b3686b25fe7ef7
Ruby Dong <ruby.dong@stonybrook.edu>
2017-09-09 15:36:52 -0400

    Fixed duplicate messages, object printing

--- index.js
+++ index.js
@@ -23,9 +23,7 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('message', function(message) {
     log('Client said: ', message);
-    // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
-    io.emit('message', message);
+    socket.emit('message', message);
   });
 
   socket.on('create or join', function(room) {
