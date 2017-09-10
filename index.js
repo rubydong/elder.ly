cced86e6237080186bee87146963df9ba474714c
Ruby Dong <ruby.dong@stonybrook.edu>
2017-09-10 04:29:04 -0400

    test.js -> index.js

--- index.js
+++ index.js
@@ -1,66 +1,70 @@
-'use strict';
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var port = process.env.PORT || 3000;
 
-var os = require('os');
-var nodeStatic = require('node-static');
-var http = require('http');
-var socketIO = require('socket.io');
+var rooms = [];
+var maxPerson = 2;
+for (var i = 0; i < 50; i++) {
+  rooms.push({ id: i, numPeople: 0 });
+}
 
+app.use(express.static("public"));
 
-var fileServer = new(nodeStatic.Server)();
-var app = http.createServer(function(req, res) {
-  fileServer.serve(req, res);
-}).listen(8080);
-
-var io = socketIO.listen(app);
-io.sockets.on('connection', function(socket) {
-
-  // convenience function to log server messages on the client
-  function log() {
-    var array = ['Message from server:'];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
-  }
-
-  socket.on('message', function(message) {
-    log('Client said: ', message);
-    socket.emit('message', message);
-  });
-
-  socket.on('create or join', function(room) {
-    log('Received request to create or join room ' + room);
-
-    var numClients = io.sockets.sockets.length;
-    log('Room ' + room + ' now has ' + numClients + ' client(s)');
-
-    if (numClients === 1) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-
-    } else if (numClients === 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      io.sockets.in(room).emit('join', room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
-    } else { // max two clients
-      socket.emit('full', room);
-    }
-  });
-
-  socket.on('ipaddr', function() {
-    var ifaces = os.networkInterfaces();
-    for (var dev in ifaces) {
-      ifaces[dev].forEach(function(details) {
-        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-          socket.emit('ipaddr', details.address);
-        }
-      });
-    }
-  });
-
-  socket.on('bye', function(){
-    console.log('received bye');
-  });
-
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/chat", function(req, res) {
+  res.sendFile(__dirname + "/design.html");
+});
+
+io.on("connection", function(socket) {
+  // Handles chat messages to rooms
+  socket.on("chat message", function(msg) {
+    console.log(msg);
+    socket.broadcast.to(socket.room).emit("chat message", msg);
+  });
+
+  // Adds user to room
+  socket.on("adduser", function(name) {
+    console.log("adding " + name);
+    socket.name = name;
+
+    var room = findRoom();
+
+    if (room === null) {
+      console.log("ROOMS ARE FULL!!");
+    }
+
+    socket.room = room.id;
+    socket.join(room.id);
+    socket.emit("room", room.id);
+//    console.log(rooms);
+  });
+
+  socket.on("disconnect", function() {
+    socket.leave(socket.room);
+    if (rooms[socket.room]) rooms[socket.room].numPeople--;
+//    console.log(rooms);
+  });
+});
+
+http.listen(port, function() {
+  console.log("listening on *:" + port);
+});
+
+function printRooms() {
+  for (var i = 0; i < 50; i++) {
+    console.log(rooms[i]);
+  }
+}
+function findRoom() {
+  for (var i = 0; i < 50; i++) {
+    if (rooms[i].numPeople < maxPerson) {
+      rooms[i].numPeople++;
+      return rooms[i];
+    }
+  }
+}
